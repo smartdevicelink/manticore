6b2cb2242d88b19015de7b362878849b23e87429
Christopher Rokita <chris@livio.io>
2016-08-24 11:11:30 -0400

    Add more API calls

--- server/app/controller.js
+++ server/app/controller.js
@@ -1,13 +1,61 @@
 //the client needs an agent to connect to so that it may access consul services
 //supply a host IP address
-var consuler = require('consul-helper')("192.168.1.144"); //start a consul client
+var consuler = require('consul-helper')(process.env.HOST_IP); //start a consul client
 var nomader = require('nomad-helper');
 var fs = require('fs');
 
 module.exports = function (app) {
-	app.post('/cores', function (req, res) {
+	//start core and hmi
+	app.post('/v1/cores', function (req, res) {
 		startWatches();
-		createCoreJob().submitJob("192.168.1.142:4646");
+		createCoreJob().submitJob(process.env.NOMAD_SERVER_IP + ":4646");
+		res.sendStatus(200);
+	});
+
+	//get a list of HMIs and their branches
+	app.get('/v1/hmis', function (req, res) {
+		let hmis = {
+			hmis: [
+				{
+					name: "ford",
+					branches: ["master", "develop"]
+				},
+				{
+					name: "generic",
+					branches: ["master"]
+				}
+			]
+		};
+		res.json(hmis);
+	});
+
+	//given an HMI, get all valid core branches
+	app.get('/v1/cores/:hmiName', function (req, res) {
+		//do something with req.params.hmiName
+		let branches = {
+			branches: [
+				"master",
+				"develop"
+			]
+		}
+		res.json(branches);
+	});
+
+	//given a core branch, get all valid build configurations
+	app.get('/v1/builds/:coreBranchName', function (req, res) {
+		//do something with req.params.coreBranchName
+		let builds = {
+			builds: [
+				"TIME_TESTER",
+				"BUILD_BT_SUPPORT"
+			]
+		}
+		res.json(builds);
+	});
+
+	//delete a core passing in an id
+	app.delete('/v1/cores/:id', function (req, res) {
+		//do something with req.params.id
 		res.sendStatus(200);
 	});
 }
@@ -24,7 +72,7 @@ function startWatches () {
 		if (services.length > 0) {
 			var jobService = services[0];
 			//this creates an sdl hmi job file suitable for manticore
-			createHmiJob(jobService.Address, jobService.Port).submitJob("192.168.1.142:4646");
+			createHmiJob(jobService.Address, jobService.Port).submitJob(process.env.NOMAD_SERVER_IP + ":4646");
 		}
 	});
 
