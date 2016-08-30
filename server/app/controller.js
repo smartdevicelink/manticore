1a74e8034fed814b17f35d0439353dc96e0924ae
Christopher Rokita <chris@livio.io>
2016-08-30 17:25:45 -0400

    Make usable for demonstration

--- server/app/controller.js
+++ server/app/controller.js
@@ -3,12 +3,17 @@
 var consuler = require('consul-helper')(process.env.NOMAD_IP_http); //start a consul client
 var nomader = require('nomad-helper');
 var fs = require('fs');
+var uuid = require('node-uuid');
+var needle = require('needle');
 
 module.exports = function (app) {
+	//set up watches one time. listen forever for changes in consul's services
+	startWatches();
 	//start core and hmi
 	app.post('/v1/cores', function (req, res) {
-		startWatches();
-		createCoreJob().submitJob(process.env.NOMAD_IP_http + ":5656");
+		//pretend we have some unique identifier for the client so that
+		//we know which client wants what core
+		requestCore(uuid.v4(), req.body);
 		res.sendStatus(200);
 	});
 
@@ -61,39 +66,91 @@ module.exports = function (app) {
 }
 
 function startWatches () {
-	//set up a watch so we know when the core job is actually running
-	consuler.watchService("core-master", function (services) {
-		//services updated. get information about core and hmi if possible
-		for (let i in services) {
-			console.log("Core " + i + " TCP Address: " + services[i].Address + ":" + services[i].Tags[0]);
-		}
-
-		//submit a corresponding hmi job file that connects with the core service
-		if (services.length > 0) {
-			var jobService = services[0];
-			//this creates an sdl hmi job file suitable for manticore
-			createHmiJob(jobService.Address, jobService.Port).submitJob(process.env.NOMAD_IP_http + ":5656");
-		}
+	consuler.watchKVStore("manticore", function (k) {
+		//change happened
+		console.log(k);
 	});
-
-	//set up a watch so we know when the hmi job is actually running
-	consuler.watchService("hmi-master", function (services) {
+	//try posting something
+	consuler.setKeyValue("manticore/testing", "123");
+	//set up a watch for all services
+	consuler.watchServices(function (services) {
 		//services updated. get information about core and hmi if possible
-		for (let i in services) {
-			console.log("HMI " + i + " user Address: " + services[i].Address + ":" + services[i].Port);
+		let cores = services.filter("core-master");
+		let hmis = services.filter("hmi-master");
+		//variable that determines whether we should wait for consul to update its services
+		//should be true if we submit or remove or change jobs that consul sees
+		let expectChangeState = false;
+		//for every core service, ensure it has a corresponding HMI. If it doesn't have one, make one
+		for (let i = 0; i < cores.length; i++) {
+			let foundHmi = false;
+			for (let j = 0; j < hmis.length; j++) {
+				//0 index has the uuid for both jobs
+				if (cores[i].Tags[0] === hmis[j].Tags[0]) {
+					j = hmis.length; //there is a match. okay
+					foundHmi = true;
+				}
+			}
+			if (!foundHmi) {
+				//make a new HMI job
+				//this creates an sdl hmi job file suitable for manticore
+				//pass in the uuid, which should be the first tag
+				createHmiJob(cores[i].Address, cores[i].Port, cores[i].Tags[0]).submitJob(process.env.NOMAD_IP_http + ":5656");
+				expectChangeState = true;
+			}
 		}
+		//now check all HMIs. if they don't have a paired core, destroy it
+		
+		for (let i = 0; i < hmis.length; i++) {
+			let foundCore = false;
+			for (let j = 0; j < cores.length; j++) {
+				//0 index has the uuid for both jobs
+				if (hmis[i].Tags[0] === cores[j].Tags[0]) {
+					foundCore = true; //match. okay
+					j = cores.length;
+				}
+				if (!foundCore) {
+					//remove. not done yet. sh
+				}
+			}
+		}
+
+		//if we expect the services to be updated then we expect this function to be called again
+		//with updated info. so don't do anything
+		if (!expectChangeState) {
+			//this should be the newest info. return connection information about the instance
+			//back to the user
+			if (cores.length > 0 && hmis.length > 0) {
+				var body = {
+					tcpAddress: cores[0].Address + ":" + cores[0].Tags[1],
+					hmiAddress: hmis[0].Address + ":" + hmis[0].Port
+				}
+				needle.post('192.168.1.142:3000/v1/address', body, function (err, res) {
+					//nothing
+				});
+			}
+
+
+
+		}
+		//services updated. get information about core and hmi if possible
+		/*for (let i = 0; i < hmis.length; i++) {
+			console.log("Core " + i + " TCP Address: " + cores[i].Address + ":" + cores[i].Tags[1]);
+			console.log("HMI " + i + " user Address: " + hmis[i].Address + ":" + hmis[i].Port);
+		}*/
 	});
 }
 
-function createHmiJob (address, port) {
+function createHmiJob (address, port, uuid) {
 	var job = nomader.createJob("hmi");
 	job.addGroup("hmi");
 	job.addTask("hmi", "hmi-master");
 	job.setImage("hmi", "hmi-master", "crokita/discovery-sdl-hmi:master");
 	job.addPort("hmi", "hmi-master", true, "user", 8080);
 	job.addEnv("hmi", "hmi-master", "HMI_WEBSOCKET_ADDR", address + ":" + port);
-	job.addService("hmi", "hmi-master", "${TASKGROUP}-hmi");
+	job.addService("hmi", "hmi-master", "hmi-master");
 	job.setPortLabel("hmi", "hmi-master", "hmi-master", "user");
+	//give hmi the same uuid as core so we know they're together
+	job.addTag("hmi", "hmi-master", "hmi-master", uuid);
 	return job;
 }
 
@@ -107,7 +164,17 @@ function createCoreJob () {
 	job.addPort("core", "core-master", true, "tcp", 12345);
 	job.addEnv("core", "core-master", "DOCKER_IP", "${NOMAD_IP_hmi}");
 	job.addService("core", "core-master", "core-master");
+	//generate a unique id for each service for pairing purposes
+	job.addTag("core", "core-master", "core-master", uuid.v4());
 	job.addTag("core", "core-master", "core-master", "${NOMAD_PORT_tcp}");
 	job.setPortLabel("core", "core-master", "core-master", "hmi");
 	return job;
+}
+
+function requestCore (uuid, body) {
+	console.log(uuid);
+	console.log(body);
+	//store the uuid in the database so that the response back
+	//will be directed towards the user making the request
+	createCoreJob().submitJob(process.env.NOMAD_IP_http + ":5656");
 }
