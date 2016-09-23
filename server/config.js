c40cf9e37925d550f5fe55cb483fafd555b7922e
Christopher Rokita <chris@livio.io>
2016-09-23 10:39:28 -0400

    Forgot a comma in config.js

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	//ip of the local consul client agent
-	consulIp: "127.0.0.1"
+	consulIp: "127.0.0.1",
 	//the address and route of the service which is supposed to send the connection information to the user
 	postConnectionAddr: "127.0.0.1:3000/v1/address",
 	//the name of your domain that you use to direct users to local ip addresses of cores and hmis
