05b9a610f52c9b46d7b5e54368e91d4508931a89
Christopher Rokita <chris@livio.io>
2016-10-17 14:57:02 -0400

    Test tcp

--- server/lib/NginxTemplate.js
+++ server/lib/NginxTemplate.js
@@ -62,7 +62,7 @@ NginxTemplate.prototype.tcp = function (port, prefix, proxyAddr) {
     }
     var serverString = `
 server {
-    listen 12345;
+    listen 1234.m.livio.io:12345;
     proxy_pass ${proxyAddr};
 `;
     this.file += serverString;
