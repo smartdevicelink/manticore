c37ae0b0ceba488847ef45684fbf977931bac506
Christopher Rokita <chris@livio.io>
2016-11-01 13:35:02 -0400

    Fix bug with haproxy clones

--- server/lib/shell.js
+++ server/lib/shell.js
@@ -132,15 +132,14 @@ module.exports = {
 			    		logger.error(err);
 			    	}
 			    	//done! reload HAProxy
-			    	console.log(file);
-			    	exec("sudo service haproxy reload", function (err, stdout, stderr) {
+			    	/*exec("sudo service haproxy reload", function (err, stdout, stderr) {
 			    		if (stdout) {
 			    			logger.debug(stdout);
 			    		}
 			    		if (stderr) {
 			    			logger.error(stderr);
 			    		}
-			    	});
+			    	});*/
 			    }); 
 
 			}, function () {//HAPROXY_OFF is set to true. do nothing
