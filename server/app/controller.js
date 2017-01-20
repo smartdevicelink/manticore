f05bf4a63c9d80652c1fc74e5c5335dab8fc4724
Christopher Rokita <chris@livio.io>
2017-01-20 15:32:01 -0500

    Fix authorization issues

--- server/app/controller.js
+++ server/app/controller.js
@@ -3,6 +3,7 @@
 var shell = require('../lib/shell.js');
 var uuid = require('node-uuid');
 var logger = require('../lib/logger');
+var jwt = require('jsonwebtoken');
 
 module.exports = function (app, io) {
 	//connect to the consul agent
@@ -13,18 +14,19 @@ module.exports = function (app, io) {
 	});
 
 	//start core and hmi
-	app.post('/v1/cores', jwtGetUserData, function (req, res) {
+	app.post('/v1/cores', function (req, res) {
 		//pretend we have some unique identifier for the client so that
 		//we know which client wants what core
 		logger.debug("/v1/cores");
 		logger.debug(req.body);
+		logger.debug(JSON.stringify(req.user, null, 4));
 		shell.requestCore(req.body.id, req.body);
 		var response = shell.requestConnection(req.body.id);
 		res.send(response);
 	});
 
 	//get logs from core
-	app.post('/v1/logs', jwtGetUserData, function (req, res) {
+	app.post('/v1/logs', function (req, res) {
 		logger.debug("/v1/logs");
 		logger.debug(req.body);
 		shell.requestLogs(req.body.id);
@@ -32,7 +34,7 @@ module.exports = function (app, io) {
 	});
 
 	//get a list of HMIs and their branches
-	app.get('/v1/hmis', jwtGetUserData, function (req, res) {
+	app.get('/v1/hmis', function (req, res) {
 		let hmis = {
 			hmis: [
 				{
@@ -45,7 +47,7 @@ module.exports = function (app, io) {
 	});
 
 	//given an HMI, get all valid core branches
-	app.get('/v1/cores/:hmiName', jwtGetUserData, function (req, res) {
+	app.get('/v1/cores/:hmiName', function (req, res) {
 		//do something with req.params.hmiName
 		let branches = {
 			branches: [
@@ -56,7 +58,7 @@ module.exports = function (app, io) {
 	});
 
 	//given a core branch, get all valid build configurations
-	app.get('/v1/builds/:coreBranchName', jwtGetUserData, function (req, res) {
+	app.get('/v1/builds/:coreBranchName', function (req, res) {
 		//do something with req.params.coreBranchName
 		let builds = {
 			builds: [
@@ -68,37 +70,8 @@ module.exports = function (app, io) {
 	});
 
 	//delete a core passing in an id
-	app.delete('/v1/cores/:id', jwtGetUserData, function (req, res) {
+	app.delete('/v1/cores/:id', function (req, res) {
 		//do something with req.params.id
 		res.sendStatus(200);
 	});
-}
-
-//if a JWT token is expected, extract the user data from the payload
-function jwtGetUserData (req, res, next) {
-	// check header or url parameters or post parameters for token
-	if (process.env.JWT_SECRET) {
-		var token = req.body.token || req.query.token || req.headers['x-access-token'];
-
-		if (token) {
-			//check the authenticity of the token using the shared secret
-			jwt.verify(token, process.env.JWT_SECRET, function (err, data) {      
-				if (err) {
-					res.status(401).send({ error: 'Authentication failure' }); //failed authentication
-				} else {
-					req.userData = data;  
-					logger.debug("AUTHENTICATION! PARSE TOKEN DATA:");
-					logger.debug(JSON.stringify(data, null, 4));  
-					next();
-				}
-			});
-		} 
-		else {
-			res.status(401).send({ error: 'Authentication failure' }); //token doesn't even exist
-		}	
-	}
-	else { //no JWT secret shared. manticore isn't expected to verify anything
-		next();
-	}
-
 }
