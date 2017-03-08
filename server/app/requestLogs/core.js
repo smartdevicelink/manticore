ce9869545cd2ed760d5a556075cba772d8f3d166
Christopher Rokita <chris@livio.io>
2017-03-08 14:20:24 -0500

    Add JSDoc and Swagger documentation, remove unused functions

--- server/app/requestLogs/core.js
+++ server/app/requestLogs/core.js
@@ -1,4 +1,12 @@
+/** @module app/requestLogs/core */
+
 module.exports = {
+	/**
+	* Finds an allocation for a specific user for sdl_core
+	* @param {array} allocations - An array of allocation objects from Nomad
+	* @param {string} targetID - The user ID
+	* @returns {object} - A found allocation object. May be null.
+	*/
 	findAliveCoreAllocation: function (allocations, targetID) {
 		for (let i = 0; i < allocations.length; i++) {
 			//remove "core-group-" from taskgroup name to get just the ID
