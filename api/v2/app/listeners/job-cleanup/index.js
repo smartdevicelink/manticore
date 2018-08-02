e382054ffb88176dd8dd379a12bed7a1f8c6bd0b
Christopher Rokita <chris@livio.io>
2018-08-02 11:14:27 -0400

    Send notification to client if their job dies

--- api/v2/app/listeners/job-cleanup/index.js
+++ api/v2/app/listeners/job-cleanup/index.js
@@ -1,5 +1,5 @@
 const config = require('../../config.js');
-const {job, logger} = config;
+const {job, logger, websocket} = config;
 const utils = require('../../utils');
 
 module.exports = {
@@ -11,6 +11,12 @@ module.exports = {
                 logger.debug(`Stopping job of user ${id}`); //core-hmi-${id}
                 const jobName = await job.idToJobName(id);
                 await utils.stopJob(jobName, true); //purge the job from Nomad
+                //inform the client that the job is now gone
+                const msgObj = {
+                    type: "dead",
+                    data: {}
+                };
+                await websocket.send(id, JSON.stringify(msgObj));
             }
         }
         next();
