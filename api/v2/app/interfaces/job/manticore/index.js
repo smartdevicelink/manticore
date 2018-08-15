a83bc45f04cfa12490460990c37cabcd66cb2c66
Christopher Rokita <chris@livio.io>
2018-08-15 14:02:20 -0400

    Fix typo

--- api/v2/app/interfaces/job/manticore/index.js
+++ api/v2/app/interfaces/job/manticore/index.js
@@ -234,9 +234,9 @@ function getUsedTcpPorts (waitingState) {
     for (let id in waitingState) {
         const cond1 = waitingState[id].services !== undefined;
         const cond2 = waitingState[id].services.core !== undefined;
-        const cond3 = waitingState[id].services.core[`core-tcp-${id}`].external !== undefined;
+        const cond3 = waitingState[id].services.core[`core-tcp-${id}-0`].external !== undefined;
         if (cond1 && cond2 && cond3) {
-            usedPorts.push(waitingState[id].services.core[`core-tcp-${id}`].external);
+            usedPorts.push(waitingState[id].services.core[`core-tcp-${id}-0`].external);
         }
     }
     return usedPorts;
