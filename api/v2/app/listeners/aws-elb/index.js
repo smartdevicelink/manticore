27441885e9167ab5ec911d2b16d39970e88f8f99
Christopher Rokita <chris@livio.io>
2018-09-19 16:31:04 -0400

    Don't let aws-elb hold up the middleware stack

--- api/v2/app/listeners/aws-elb/index.js
+++ api/v2/app/listeners/aws-elb/index.js
@@ -4,9 +4,9 @@ const AwsHandler = require('../../AwsHandler.js')();
 
 module.exports = {
 	"post-waiting-job-advance": async (ctx, next) =>{
+		next(); //don't hold up the stack. there are known problems with the AWS API not responding quickly
 		if (config.modes.elb) {
 			await AwsHandler.changeState(ctx.waitingState);
 		}
-		next();
 	}
 }
