c271cb2734480973a31dc4de182fcacd84171d16
Christopher Rokita <chris@livio.io>
2019-12-02 13:35:39 -0500

    Update HMI job version

--- api/v2/app/interfaces/job/manticore/index.js
+++ api/v2/app/interfaces/job/manticore/index.js
@@ -24,7 +24,7 @@ const jobInfo = {
     }, 
     hmis: [{
         type: "generic",
-        versions: ["minimal-0.7.0"] //ex. master, minimal-0.5.1
+        versions: ["minimal-0.7.2"] //ex. master, minimal-0.5.1
     }]
 };
 
@@ -298,7 +298,7 @@ function exampleJobOption () {
         },
         hmi: {
             type: "generic",
-            version: "minimal-0.7.0"
+            version: "minimal-0.7.2"
         }
     };
 }
